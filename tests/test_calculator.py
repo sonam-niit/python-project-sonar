68f6580f42a64b9063d90729528897a4ec4e9d18
sonam-niit <sonam.gravity@gmail.com>
2025-06-14 10:43:28 +0530

    basic test

---
+++ tests/test_calculator.py
@@ -0,0 +1,18 @@
+import pytest
+from app.calculator import add,subtract,multiply,divide
+
+def test_add():
+    assert add(2,3)==5
+    
+def test_subtract():
+    assert subtract(10,2)==8
+    
+def test_multiply():
+    assert multiply(3,9)==27
+    
+def test_divide():
+    assert divide(20,10)==2
+    
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10,0)
